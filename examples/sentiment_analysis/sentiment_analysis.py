391fd0a91477530d851054752ee95cc3fda60159
ruanchaves <ruanchaves93@gmail.com>
2021-07-21 15:29:04 -0300

    logging

--- examples/sentiment_analysis/sentiment_analysis.py
+++ examples/sentiment_analysis/sentiment_analysis.py
@@ -12,7 +12,6 @@ from datasets import (
 
 import transformers
 from transformers import (
-    AutoConfig,
     pipeline,
     AutoTokenizer,
     AutoModelForSequenceClassification,
@@ -37,6 +36,22 @@ class DataArguments:
         default='./tass_reader.py'
     )
 
+    load_from_disk: bool = field(
+        default=False
+    )
+
+    save_to_disk: bool = field(
+        default=False
+    )
+
+    dataset_load_path: Optional[str] = field(
+        default=None
+    )
+
+    dataset_save_path: Optional[str] = field(
+        default=None
+    )
+
     dataset_url: str = field(
         default=None
     )
@@ -53,14 +68,6 @@ class DataArguments:
         default = "polarity"
     )
 
-    output_dir: str = field(
-        default="./output"
-    )
-
-    load_cache_from_output_dir: bool = field(
-        default=True
-    )
-
     sample: Optional[int] = field(
         default=None
     )
@@ -71,6 +78,10 @@ class DataArguments:
 
 @dataclass
 class TextClassificationArguments:
+
+    run_classifier: bool = field(
+        default=True
+    )
 
     sentiment_model: str = field(
         default="finiteautomata/beto-sentiment-analysis"
@@ -94,6 +105,10 @@ class TextClassificationArguments:
 
 @dataclass
 class WordSegmenterArguments:
+
+    run_segmenter: bool = field(
+        default=True
+    )
 
     decoder_model_name_or_path: str = field(
         default="DeepESP/gpt2-spanish"
@@ -132,6 +147,51 @@ def rgetattr(obj, attr, *args):
         return getattr(obj, attr, *args)
     return functools.reduce(_getattr, [obj] + attr.split('.'))
 
+def process_rows(batch, classifier=None, content_field="content", predictions_field="predictions"):
+    sentences = batch[content_field]
+    labels = classifier(sentences)
+    batch.update({predictions_field: labels})
+    return batch
+
+def segment_content(batch, segmenter=None, content_field="content", segmented_content_field="segmented_content"):
+    sentences = batch[content_field]
+    segmented_content = segmenter.process_hashtags(sentences)
+    segmented_content = [ " ".join(x) for x in segmented_content]
+    batch.update({segmented_content_field: segmented_content})
+    return batch
+
+def eval_dataset(
+    data,
+    split="test",
+    reference_field="polarity",
+    predictions_field="predictions",
+    metric="./sentiment_metrics.py"):
+    predictions = data[split][predictions_field]
+    predictions = [x["label"] for x in predictions]
+    references = data[split][reference_field]
+    metric = load_metric(metric)
+    eval_results = metric.compute(
+        predictions=predictions,
+        references=references
+    )
+    return eval_results
+
+
+def deleteEncodingLayers(model, num_layers_to_keep):  # must pass in the full bert model
+    model_type = rgetattr(model, "config.model_type")
+    oldModuleList = rgetattr(model, f"{model_type}.encoder.layer")
+    newModuleList = nn.ModuleList()
+
+    # Now iterate over all layers, only keepign only the relevant layers.
+    for i in range(0, num_layers_to_keep):
+        newModuleList.append(oldModuleList[i])
+
+    # create a copy of the model, modify it with the new list, and return
+    copyOfModel = copy.deepcopy(model)
+    rsetattr(copyOfModel, f"{model_type}.encoder.layer", newModuleList)
+
+    return copyOfModel
+
 def main():
     parser = HfArgumentParser((TextClassificationArguments, WordSegmenterArguments, DataArguments))
     if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
@@ -156,42 +216,33 @@ def main():
     transformers.utils.logging.enable_default_handler()
     transformers.utils.logging.enable_explicit_format()
 
-    ws = WordSegmenter(
-        decoder_model_name_or_path=ws_args.decoder_model_name_or_path,
-        decoder_model_type=ws_args.decoder_model_type,
-        decoder_device=ws_args.decoder_device,
-        decoder_gpu_batch_size=ws_args.decoder_gpu_batch_size,
-        encoder_model_name_or_path=ws_args.encoder_model_name_or_path,
-        encoder_model_type=ws_args.encoder_model_type,
-        spacy_model=ws_args.spacy_model
-    )
+    if ws_args.run_segmenter:
+        ws = WordSegmenter(
+            decoder_model_name_or_path=ws_args.decoder_model_name_or_path,
+            decoder_model_type=ws_args.decoder_model_type,
+            decoder_device=ws_args.decoder_device,
+            decoder_gpu_batch_size=ws_args.decoder_gpu_batch_size,
+            encoder_model_name_or_path=ws_args.encoder_model_name_or_path,
+            encoder_model_type=ws_args.encoder_model_type,
+            spacy_model=ws_args.spacy_model
+        )
+    else:
+        ws = None
 
-    def deleteEncodingLayers(model, num_layers_to_keep):  # must pass in the full bert model
-        model_type = rgetattr(model, "config.model_type")
-        oldModuleList = rgetattr(model, f"{model_type}.encoder.layer")
-        newModuleList = nn.ModuleList()
+    if class_args.run_classifier:
+        model = AutoModelForSequenceClassification.from_pretrained(class_args.sentiment_model)
+        if class_args.prune_layers:
+            model = deleteEncodingLayers(model, class_args.prune_layers)
+        tokenizer = AutoTokenizer.from_pretrained(class_args.sentiment_model)
+        classifier = pipeline("sentiment-analysis", 
+            model=model,
+            tokenizer=tokenizer,
+            device=class_args.sentiment_model_device)
 
-        # Now iterate over all layers, only keepign only the relevant layers.
-        for i in range(0, num_layers_to_keep):
-            newModuleList.append(oldModuleList[i])
-
-        # create a copy of the model, modify it with the new list, and return
-        copyOfModel = copy.deepcopy(model)
-        rsetattr(copyOfModel, f"{model_type}.encoder.layer", newModuleList)
-
-        return copyOfModel
-
-    model = AutoModelForSequenceClassification.from_pretrained(class_args.sentiment_model)
-    if class_args.prune_layers:
-        model = deleteEncodingLayers(model, class_args.prune_layers)
-
-    tokenizer = AutoTokenizer.from_pretrained(class_args.sentiment_model)
-    classifier = pipeline("sentiment-analysis", 
-        model=model,
-        tokenizer=tokenizer,
-        device=class_args.sentiment_model_device)
-
-    data = load_dataset(data_args.dataset_reader, url=data_args.dataset_url)
+    if data_args.load_from_disk:
+        data = datasets.load_from_disk(data_args.dataset_load_path)
+    else:
+        data = load_dataset(data_args.dataset_reader, url=data_args.dataset_url)
 
     if data_args.hashtag_only:
         data = data.filter(lambda x: x["has_hashtag"])
@@ -202,43 +253,40 @@ def main():
 
     split_length = data[data_args.split].shape[0]
 
-    def process_rows(batch, classifier=None, content_field="content", predictions_field="predictions"):
-        sentences = batch[content_field]
-        labels = classifier(sentences)
-        batch.update({predictions_field: labels})
-        return batch
+    if class_args.run_classifier:
+        data = data.map(
+            process_rows, 
+            fn_kwargs={
+                "classifier": classifier,
+                "content_field": data_args.content_field
+            },
+            batched=True, 
+            batch_size=class_args.batch_size)
 
-    def segment_content(batch, segmenter=None, content_field="content", segmented_content_field="segmented_content"):
-        sentences = batch[content_field]
-        segmented_content = segmenter.process_hashtags(sentences)
-        segmented_content = [ " ".join(x) for x in segmented_content]
-        batch.update({segmented_content_field: segmented_content})
-        return batch
-
-    def eval_dataset(
-        data,
-        split="test",
-        reference_field="polarity",
-        predictions_field="predictions",
-        metric="./sentiment_metrics.py"):
-        predictions = data[split][predictions_field]
-        predictions = [x["label"] for x in predictions]
-        references = data[split][reference_field]
-        metric = load_metric(metric)
-        eval_results = metric.compute(
-            predictions=predictions,
-            references=references
+    if class_args.run_segmenter:
+        data = data.map(
+            segment_content,
+            fn_kwargs={
+                "segmenter": ws,
+                "content_field": data_args.content_field
+            },
+            batched=True, 
+            batch_size=split_length
         )
-        return eval_results
 
-    data = data.map(
-        process_rows, 
-        fn_kwargs={
-            "classifier": classifier,
-            "content_field": data_args.content_field
-        },
-        batched=True, 
-        batch_size=class_args.batch_size)
+    if class_args.run_classifier:
+        data = data.map(
+            process_rows, 
+            fn_kwargs={
+                "classifier": classifier,
+                "content_field": "segmented_content",
+                "predictions_field": "segmented_predictions"
+            },
+            batched=True, 
+            batch_size=class_args.batch_size)
+
+    if data_args.save_to_disk:
+        data.save_to_disk(data_args.dataset_save_path)
 
     if data_args.hashtag_only:
         subset_evaluation = eval_dataset(
@@ -251,47 +299,27 @@ def main():
         subset_evaluation = eval_dataset(data_subset)
         full_evaluation = eval_dataset(data)
 
-    data = data.map(
-        segment_content,
-        fn_kwargs={
-            "segmenter": ws,
-            "content_field": data_args.content_field
-        },
-        batched=True, 
-        batch_size=split_length
-    )
-
-    data = data.map(
-        process_rows, 
-        fn_kwargs={
-            "classifier": classifier,
-            "content_field": "segmented_content",
-            "predictions_field": "segmented_predictions"
-        },
-        batched=True, 
-        batch_size=class_args.batch_size)
-
     if data_args.hashtag_only:
-
         subset_evaluation_after_segmentation = eval_dataset(
             data,
             predictions_field="segmented_predictions"
         )
-        
         full_evaluation_after_segmentation = None
-    
     else:
         data_subset = data.filter(lambda x: x['has_hashtag'])
-
         subset_evaluation_after_segmentation = eval_dataset(
             data_subset,
             predictions_field="segmented_predictions"
         )
-
         full_evaluation_after_segmentation = eval_dataset(
             data,
             predictions_field="segmented_predictions"
         )
+
+    log_args = {}
+    for item in [class_args, ws_args, data_args]:
+        log_args.update(vars(item))
+    logger.info("%s", log_args)
 
     logger.info("Full dataset evaluation:")
     logger.info("%s", full_evaluation)
