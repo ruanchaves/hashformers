92525f463577a54c300f7750e8895dcf6bd58dc1
ruanchaves <ruanchaves93@gmail.com>
2021-07-21 22:43:03 -0300

    sentiment analysis pruning experiments

--- examples/sentiment_analysis/pruning_experiments.sh
+++ examples/sentiment_analysis/pruning_experiments.sh
@@ -9,16 +9,16 @@ python sentiment_analysis.py \
     --decoder_model_type gpt2 \
     --encoder_model_name_or_path bert-large-uncased-whole-word-masking \
     --encoder_model_type bert \
-    --spacy_model en_core_web_sm
+    --spacy_model en_core_web_sm \
+    --sample 3
 
 for i in {1..24}:
 do
     python sentiment_analysis.py \
     --log_level INFO \
-    --dataset_reader semeval2017.py \
-    --hashtag_only \
     --dataset_load_path semeval2017 \
     --run_segmenter False \
+    --sentiment_model siebert/sentiment-roberta-large-english \
     --do_eval \
     --prune_layers $i
 done
