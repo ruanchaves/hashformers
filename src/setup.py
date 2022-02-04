1a53fd5e11d75c7de8c1bf73460b2acaeb99b47d
ruanchaves <ruanchaves93@gmail.com>
2022-02-04 03:52:46 +0100

    update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
    name='word_segmentation',
@@ -6,4 +6,6 @@ setup(
    author='Ruan Chaves Rodrigues',
    author_email='ruanchave93@gmail.com',
    description='Word segmentation with transformers',
+   packages=find_packages('src'),
+   package_dir={'': 'src'},
 )
