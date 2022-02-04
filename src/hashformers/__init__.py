578a7ab1c694b441e56106e55fbcfb1f7db2b740
ruanchaves <ruanchaves93@gmail.com>
2022-02-04 23:50:50 +0100

    docs

--- src/hashformers/__init__.py
+++ src/hashformers/__init__.py
@@ -1,1 +1,4 @@
+"""
+.. include:: ../../README.md
+"""
 from .segmenter import *
--- src/hashformers/segmenter.py
+++ src/hashformers/segmenter.py
@@ -2,6 +2,7 @@ from hashformers.beamsearch.algorithm import Beamsearch
 from hashformers.beamsearch.reranker import Reranker
 from hashformers.beamsearch.data_structures import enforce_prob_dict
 from hashformers.ensemble.top2_fusion import top2_ensemble
+from typing import List, Union, Any
 
 class WordSegmenter(object):
 
@@ -15,15 +16,18 @@ class WordSegmenter(object):
         reranker_model_name_or_path = "bert-base-uncased",
         reranker_model_type = "bert"
     ):
-        """Segments a list of hashtags.
+        """Word segmentation API initialization. 
+           A GPT-2 model must be passed to `segmenter_model_name_or_path`, and optionally a BERT model to `reranker_model_name_or_path`.
+           If `reranker_model_name_or_path` is set to `False` or `None`, the word segmenter object will work without a reranker.
+
 
         Args:
-            segmenter_model_name_or_path (str, optional): Transformer decoder model name. Defaults to "gpt2".
+            segmenter_model_name_or_path (str, optional): GPT-2 that will be fetched from the Hugging Face Model Hub. Defaults to "gpt2".
             segmenter_model_type (str, optional): Transformer decoder model type. Defaults to "gpt2".
             segmenter_device (str, optional): Device. Defaults to "cuda".
             segmenter_gpu_batch_size (int, optional): Segmenter GPU batch size. Defaults to 1.
             reranker_gpu_batch_size (int, optional): Reranker GPU split size. Defaults to 2000.
-            reranker_model_name_or_path (str, optional): Transformer encoder model name. Defaults to "bert-base-uncased".
+            reranker_model_name_or_path (str, optional): BERT model that will be fetched from the Hugging Face Model Hub. It is possible to turn off the reranker by passing a None or False value to this argument. Defaults to "bert-base-uncased".
             reranker_model_type (str, optional): Transformer encoder model type. Defaults to "bert".
         """
         self.segmenter_model = Beamsearch(
@@ -44,29 +48,50 @@ class WordSegmenter(object):
 
     def segment(
             self,
-            word_list,
-            topk=20,
-            steps=13,
-            alpha=0.222,
-            beta=0.111,
-            use_reranker=True,
-            return_ranks=False,
-            trim_hashtags=True):
+            word_list: List[str],
+            topk: int = 20,
+            steps: int = 13,
+            alpha: float = 0.222,
+            beta: float = 0.111,
+            use_reranker: bool = True,
+            return_ranks: bool = False,
+            trim_hashtags: bool = True) -> Any :
         """Segment a list of hashtags.
 
         Args:
-            word_list (List[str]): List of hashtag strings.
-            topk (int, optional): top-k parameter for the Beamsearch algorithm. Defaults to 20.
-            steps (int, optional): steps parameter for the Beamsearch algorithm. Defaults to 13.
-            alpha (float, optional): alpha parameter for the top-2 ensemble. Defaults to 0.222.
-            beta (float, optional): beta parameter for the top-2 ensemble. Defaults to 0.111.
-            use_reranker (bool, optional): Whether or not to run the reranker and the ensemble. Defaults to True.
-            return_ranks (bool, optional): Return not just the segmented hashtags but also the ranks. Defaults to False.
-            trim_hashtags (bool, optional): Automatically remove "#" characters from the beginning of the hashtags. Defaults to True.
+            word_list (List[str]): A list of hashtag strings.
+            topk (int, optional): 
+                top-k parameter for the Beamsearch algorithm. 
+                A lower top-k value will speed up the algorithm. 
+                However, this will decrease the amount of candidate segmentations in a rank. 
+                Defaults to 20.
+            steps (int, optional): 
+                steps parameter for the Beamsearch algorithm. 
+                A lower amount of steps will speed up the algorithm. 
+                However, the algorithm will never detect a number of words larger than amount of steps. 
+                Defaults to 13. 
+            alpha (float, optional): 
+                alpha parameter for the top-2 ensemble. 
+                It controls the weight given to the segmenter candidates. 
+                Reasonable values range from 0 to 1. 
+                Defaults to 0.222.
+            beta (float, optional): 
+                beta parameter for the top-2 ensemble. 
+                It controls the weight given to the reranker candidates. 
+                Reasonable values range from 0 to 1. 
+                Defaults to 0.111.
+            use_reranker (bool, optional): 
+                Whether or not to run the reranker. 
+                Defaults to True.
+            return_ranks (bool, optional): 
+                Return not just the segmented hashtags but also the a dictionary of the ranks. 
+                Defaults to False.
+            trim_hashtags (bool, optional): 
+                Automatically remove "#" characters from the beginning of the hashtags. 
+                Defaults to True.
 
         Returns:
-            A list of segmented hashtags if return_ranks == False.
-            A dictionary of the ranks and the segmented hashtags if return_ranks == True.
+            Any: A list of segmented hashtags if return_ranks == False. A dictionary of the ranks and the segmented hashtags if return_ranks == True.
         """
 
         if trim_hashtags:
