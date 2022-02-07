f55aaad2018d4d962fc6dcfcd94dcf99a7505786
ruanchaves <ruanchaves93@gmail.com>
2022-02-07 20:49:34 +0100

    tutorial update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
    name='hashformers',
-   version='1.1.0',
+   version='1.1.1',
    author='Ruan Chaves Rodrigues',
    author_email='ruanchave93@gmail.com',
    description='Word segmentation with transformers',
