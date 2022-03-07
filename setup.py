578ce769efac2c447218dc08bcf86bfb7cc8c86d
ruanchaves <ruanchaves93@gmail.com>
2022-03-07 10:24:47 +0100

    add stan

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
    name='hashformers',
-   version='1.2.4',
+   version='1.2.5',
    author='Ruan Chaves Rodrigues',
    author_email='ruanchave93@gmail.com',
    description='Word segmentation with transformers',
